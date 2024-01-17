e709a41f8fcde518d421328c795a7917a4c217e2
Kauan <kauancph11@gmail.com>
2024-01-16 21:54:29 -0300

    16/01/24

--- app/src/main/java/com/example/vsconnect_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/vsconnect_kotlin/MainActivity.kt
@@ -26,6 +26,8 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
         binding.appBarMain.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
@@ -37,18 +39,14 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_editar_imagem, R.id.nav_servicos, R.id.nav_sair
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
+
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
